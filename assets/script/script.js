fa1765c06083fddee0aac6012bdebbbb90cc86f3
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-07-26 00:48:32 -0700

    new improved UI

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  return "mypassword";
+}
